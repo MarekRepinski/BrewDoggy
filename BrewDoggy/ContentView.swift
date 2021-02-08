e75bfc36188204c968c3ea769e34b7807eef55ce
Marek Repinski <Repinski@hotmail.com>
2021-02-08 16:10:55 +0100

    Initial commit. This project will replace "BrewDoggie"

--- BrewDoggy/ContentView.swift
+++ BrewDoggy/ContentView.swift
@@ -12,9 +12,9 @@ struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
 
     @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
+        sortDescriptors: [NSSortDescriptor(keyPath: \Recipe.timestamp, ascending: true)],
         animation: .default)
-    private var items: FetchedResults<Item>
+    private var items: FetchedResults<Recipe>
 
     var body: some View {
         List {
@@ -36,32 +36,32 @@ struct ContentView: View {
 
     private func addItem() {
         withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+//            let newItem = Item(context: viewContext)
+//            newItem.timestamp = Date()
+//
+//            do {
+//                try viewContext.save()
+//            } catch {
+//                // Replace this implementation with code to handle the error appropriately.
+//                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+//                let nsError = error as NSError
+//                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+//            }
         }
     }
 
     private func deleteItems(offsets: IndexSet) {
         withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+//            offsets.map { items[$0] }.forEach(viewContext.delete)
+//
+//            do {
+//                try viewContext.save()
+//            } catch {
+//                // Replace this implementation with code to handle the error appropriately.
+//                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+//                let nsError = error as NSError
+//                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+//            }
         }
     }
 }
---
+++ BrewDoggy/Recipe/AddRecipeView.swift
@@ -0,0 +1,48 @@
+//
+//  AddRecipeView.swift
+//  BrewDoggy
+//
+//  Created by Marek Repinski on 2021-02-08.
+//
+
+import SwiftUI
+
+struct AddRecipeView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    @State private var name = ""
+    @State private var picture = ""
+    @State private var instructions = ""
+    
+
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Name of recipe", text: $name)
+                    TextField("Picture", text: $picture)
+                }
+                Section {
+                    TextField("Instructions", text: $instructions)
+                }
+                Section {
+                    Button("Save") {
+                        let newRecipe = Recipe(context: viewContext)
+                        newRecipe.name = self.name
+                        newRecipe.picture = self.picture
+                        newRecipe.instructions = self.instructions
+                        newRecipe.timestamp = Date()
+                        
+                        try? self.viewContext.save()
+                    }
+                }
+            }
+            .navigationBarTitle("Add a recipe")
+        }
+    }
+}
+
+struct AddRecipeView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddRecipeView()
+    }
+}
